0c53f4bac801b6ebeb80be95437d21bcf11e2ded
AffectedArc07 <josephcase55@hotmail.com>
2018-07-07 15:01:05 +0100

    Adds easy embeds and cleans up old code

--- Hippie/HippieStation/Files/Map/Rename-All.py
+++
@@ -1,6 +0,0 @@
-import os
-for x in range(0,8):
-    for y in range(0,8):
-        oldName = ("Cyberiad [www.imagesplitter.net]-"+str(x)+"-"+str(y)+".png")
-        newName = (str(x)+str(y)+".png")
-        os.rename(oldName, newName)
