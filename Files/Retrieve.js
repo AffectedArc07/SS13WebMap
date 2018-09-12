4ec32380798b2f2d229fe95b8aada5ff5da69194
AffectedArc07 <josephcase55@hotmail.com>
2018-09-12 21:15:05 +0100

    Auto changelog!

---
+++ Files/Retrieve.js
@@ -0,0 +1,47 @@
+var apiURL = 'https://api.github.com/repos/AffectedArc07/SS13WebMap/commits?per_page=5&sha='
+
+/**
+ * Actual demo
+ */
+
+var demo = new Vue({
+
+    el: '#gitlog',
+
+    data: {
+        branches: ['master'],
+        currentBranch: 'master',
+        commits: null
+    },
+
+    created: function () {
+        this.fetchData()
+    },
+
+    watch: {
+        currentBranch: 'fetchData'
+    },
+
+    filters: {
+        truncate: function (v) {
+            var newline = v.indexOf('\n')
+            return newline > 0 ? v.slice(0, newline) : v
+        },
+        formatDate: function (v) {
+            return v.replace(/T|Z/g, ' ')
+        }
+    },
+
+    methods: {
+        fetchData: function () {
+            var xhr = new XMLHttpRequest()
+            var self = this
+            xhr.open('GET', apiURL + self.currentBranch)
+            xhr.onload = function () {
+                self.commits = JSON.parse(xhr.responseText)
+                console.log(self.commits[0].html_url)
+            }
+            xhr.send()
+        }
+    }
+})
